a925e03822d3438a6dbfe1168e4f5d9edcefb25f
Dugd <dugd.nitkin@gmail.com>
2025-11-19 14:36:55 +0200

    feat: auto run alembic migrations

--- src/main.py
+++ src/main.py
@@ -2,13 +2,27 @@ from fastapi import FastAPI
 from src.core import health_router
 from src.storage import storage_router
 from src.external_api import external_api_router
+from alembic.config import Config
+from alembic import command
+
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+
+run_migrations()
+
 
 app = FastAPI(
-    title="Lab3 FastAPI Project",
+    title="FastAPI Project",
     description="Lab project with FastAPI and Swagger UI",
-    version="0.1.0",
+    version="0.2.0",
 )
 
 app.include_router(health_router)
 app.include_router(storage_router)
 app.include_router(external_api_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the FastAPI Project!"}
